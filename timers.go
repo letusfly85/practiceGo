d99ec580ac4fd62b2522b504c3f72105b305dc8e
letusfly85 <letusfly95@gmail.com>
2014-07-22 19:07:18 +0900

    timers

---
+++ timers.go
@@ -0,0 +1,27 @@
+/**
+ * https://gobyexample.com/timers
+ *
+ */
+
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+
+	timer1 := time.NewTimer(time.Second * 3)
+
+	<-timer1.C
+	fmt.Println("Timer 1 expired")
+
+	timer2 := time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 expired")
+	}()
+	stop2 := timer2.Stop()
+	if stop2 {
+		fmt.Println("Timer 2 stopped")
+	}
+}
