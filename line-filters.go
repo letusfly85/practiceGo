05fef29f0dcee163c9fc3f287b28fdaccae0ba06
letusfly85 <letusfly95@gmail.com>
2014-08-08 10:46:13 +0900

    line-filters

---
+++ line-filters.go
@@ -0,0 +1,30 @@
+/**
+ * https://gobyexample.com/line-filters
+ *
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+
+		ucl := strings.ToUpper(scanner.Text())
+
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
