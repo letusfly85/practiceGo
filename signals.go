57eb916f0d42039024c11b4494834580a3a79800
letusfly85 <letusfly95@gmail.com>
2014-08-13 20:00:52 +0900

    signals

---
+++ signals.go
@@ -0,0 +1,30 @@
+/**
+ * https://gobyexample.com/signals
+ *
+ */
+
+package main
+
+import "fmt"
+import "os"
+import "os/signal"
+import "syscall"
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
