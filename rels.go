b971970b0b0feef6ffe9e4c01cfc008782cd00a4
letusfly85 <letusfly95@gmail.com>
2014-08-19 13:53:05 +0900

    rels

---
+++ rels.go
@@ -0,0 +1,27 @@
+/**
+ * http://golang.org/pkg/path/filepath/#example_Rel
+ *
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	paths := []string{
+		"/a/b/c",
+		"/b/c",
+		"./b/c",
+	}
+	base := "/a"
+
+	fmt.Println("On Unix:")
+	for _, p := range paths {
+		rel, err := filepath.Rel(base, p)
+		fmt.Printf("%q: %q %v\n", p, rel, err)
+	}
+}
