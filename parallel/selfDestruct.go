a0bd40f2c61ee19969cd0554863c9466318b1d17
shunsuke wada <letusfly95@gmail.com>
2014-09-02 22:59:13 +0900

    channel programs

---
+++ parallel/selfDestruct.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	abort := make(chan bool)
+	count := make(chan int)
+
+	go cancel(abort)
+	go countDown(count)
+
+	for {
+		select {
+		case i := <-count:
+			if 0 == i {
+				return
+			}
+			fmt.Printf("%d seconds remaining\n", i)
+		case a := <-abort:
+			if a {
+				fmt.Printf("Self destruct aborted\n")
+			} else {
+				return
+			}
+			return
+		}
+	}
+}
+
+func cancel(abort chan bool) {
+	fmt.Println("This program will self destruct, do you wish to cancel?\n")
+
+	var r int
+	fmt.Scanf("%c", &r)
+	switch r {
+	default:
+		abort <- false
+	case 'y':
+		abort <- true
+	case 'Y':
+		abort <- true
+	}
+}
+
+func countDown(count chan int) {
+	for i := 10; i >= 0; i-- {
+		count <- i
+		time.Sleep(1000 * 1000 * 1000)
+	}
+}
