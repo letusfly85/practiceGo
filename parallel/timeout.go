5d465ee12c81b5f00623453488e0b76e226233c3
shunsuke wada <letusfly95@gmail.com>
2014-09-03 23:43:58 +0900

    timeout

---
+++ parallel/timeout.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timeout(t chan bool) {
+	time.Sleep(5000 * 1000 * 1000)
+	t <- true
+}
+
+func readString(s chan string) {
+	time.Sleep(10000 * 1000 * 1000)
+	msg := "Hello, World!"
+	s <- msg
+}
+
+func countDown(count chan int) {
+	for i := 10; i >= 0; i-- {
+		count <- i
+		time.Sleep(1000 * 1000 * 1000)
+	}
+}
+
+func main() {
+	t := make(chan bool)
+	s := make(chan string)
+	c := make(chan int)
+
+	go countDown(c)
+	go readString(s)
+	go timeout(t)
+
+	for {
+		select {
+		case i := <-c:
+			fmt.Printf("%d seconds remaining\n", i)
+
+		case msg := <-s:
+			fmt.Printf("Received: %s\n", msg)
+			return
+
+		case <-t:
+			fmt.Println("Timed out\n")
+			return
+		}
+	}
+}
