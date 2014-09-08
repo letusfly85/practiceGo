9858c8aa628fb94ed6591d8a470d102016d30cb6
shunsuke wada <letusfly95@gmail.com>
2014-09-09 00:33:19 +0900

    channel関連の組み込み関数cap, lenのサンプル

--- parallel/chan.go
+++ parallel/chan.go
@@ -1,3 +1,10 @@
+/**
+ * channelの学習用ソースコード
+ *
+ * refs:
+ *  http://qiita.com/tenntenn/items/686a75e11e8dcd9912ec
+ *
+ */
 package main
 
 import "fmt"
@@ -12,4 +19,18 @@ func main() {
 	}()
 	_ = <-c
 	fmt.Printf("%s\n", m[2])
+
+	ch1 := make(chan int, 10)
+	fmt.Println(cap(ch1))
+
+	ch2 := make(chan int)
+	fmt.Println(cap(ch2))
+
+	ch3 := make(chan struct{}, 10)
+	ch3 <- struct{}{}
+
+	fmt.Println("capture:", cap(ch3), "length:", len(ch3))
+
+	<-ch3
+	fmt.Println("capture:", cap(ch3), "length:", len(ch3))
 }
