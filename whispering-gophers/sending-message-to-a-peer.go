f436b6d9a1588d3eac4956610f7e3c5c05f781b8
shunsuke wada <letusfly95@gmail.com>
2014-09-15 00:44:24 +0900

    jsonオブジェクトのアンマーシャル処理の写経

--- whispering-gophers/sending-message-to-a-peer.go
+++ whispering-gophers/sending-message-to-a-peer.go
@@ -4,6 +4,8 @@
  *
  * usage: go run sending-message-to-a-peer.go hoge
  *
+ *
+ *
  */
 
 package main
@@ -17,38 +19,32 @@ import (
 )
 import "os"
 
+type Site struct {
+	Title string
+	URL   string
+}
+
 // go run sending-messages-to-a-peer.go "message" "localhost:3333"
 func main() {
 
 	strEcho := os.Args[1]
 	servAddr := os.Args[2]
-	/*
-		tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-		if err != nil {
-			println("Resolve TCP address failed:", err.Error())
-			os.Exit(1)
-		}
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			println("Dial failed:", err.Error())
-			os.Exit(1)
-		}
-	*/
 	conn, err := net.Dial("tcp", servAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	var site = Site{Title: strEcho, URL: servAddr}
 	var b bytes.Buffer
 	enc := json.NewEncoder(&b)
-	d := map[string]string{"message": strEcho}
-	err = enc.Encode(d)
+	err = enc.Encode(site)
 	if err != nil {
 		fmt.Println(err)
 	}
-	str := b.String()
 
+	str := b.String()
+	println(str)
 	_, err = conn.Write([]byte(str))
 	if err != nil {
 		println("Write to server failed:", err.Error())
@@ -58,7 +54,7 @@ func main() {
 	reply := make([]byte, 1024)
 	_, err = conn.Read(reply)
 	if err != nil {
-		println("Write to server failed:", err.Error())
+		println("Reply From server failed:", err.Error())
 		os.Exit(1)
 	}
 	println("reply from server=", string(reply))
--- whispering-gophers/tcp-server.go
+++ whispering-gophers/tcp-server.go
@@ -2,6 +2,7 @@
  * refs: http://whispering-gophers.appspot.com/talk.slide#16
  * refs: https://coderwall.com/p/wohavg
  * refs: http://d.hatena.ne.jp/taknb2nch/20140210/1392044307
+ * refs: http://ramtiga.hatenablog.jp/entry/2013/11/06/184113
  *
  *
  */
@@ -9,9 +10,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net"
-	"os"
 )
+
+type Site struct {
+	Title string
+	URL   string
+}
 
 const (
 	CONN_HOST = "localhost"
@@ -22,34 +30,33 @@ const (
 func main() {
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
-		println("Error listening:", err.Error())
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	defer l.Close()
 
 	println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			println("Error accepting: ", err.Error())
-			os.Exit(1)
+			log.Fatal(err)
 		}
-
 		go handleRequest(conn)
 	}
 }
 
 func handleRequest(conn net.Conn) {
-	buf := make([]byte, 1024)
+	defer conn.Close()
 
+	buf := make([]byte, 1024)
 	reqLen, err := conn.Read(buf)
 	if err != nil {
 		println("Error reading:", err.Error())
 	}
 	s := string(buf[:reqLen])
 
-	conn.Write([]byte(s))
-	conn.Close()
+	site := new(Site)
+	err = json.Unmarshal([]byte(s), &site)
+	fmt.Fprintln(conn, site.URL)
+	//io.Copy(conn, conn)
 }
