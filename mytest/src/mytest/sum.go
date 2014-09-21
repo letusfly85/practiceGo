1d2b250c1862b9a90faa806e2053b97c53a35065
shunsuke wada <letusfly95@gmail.com>
2014-09-21 23:41:10 +0900

    再帰風にsumを記載してみた

--- mytest/src/mytest/sum.go
+++ mytest/src/mytest/sum.go
@@ -6,6 +6,16 @@ func MySum(xs ...int) int {
 	for _, val := range xs {
 		acc = acc + val
 	}
-
 	return acc
+}
+
+func MyFoldSum(xs []int) int {
+	switch {
+	case len(xs) == 1:
+		return xs[0]
+
+	default:
+		head, tail := xs[0], xs[1:]
+		return (head + MyFoldSum(tail))
+	}
 }
--- mytest/test/mytest/sum_test.go
+++ mytest/test/mytest/sum_test.go
@@ -11,3 +11,12 @@ func TestSum(t *testing.T) {
 		t.Errorf("got %v\nant: %v", actual, expected)
 	}
 }
+
+func TestMyFoldSum(t *testing.T) {
+	actual := mytest.MyFoldSum([]int{10, 11, 12})
+	expected := 33
+
+	if actual != expected {
+		t.Errorf("got %v\nant: %v", actual, expected)
+	}
+}
